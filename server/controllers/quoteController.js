3f35c17819bdacc9324378795bce3817716afa86
pedrodcarvalho <pedrodc51203@gmail.com>
2023-09-23 15:15:54 -0300

    Implement quoted ticker info page

--- server/controllers/quoteController.js
+++ server/controllers/quoteController.js
@@ -19,7 +19,7 @@ const getQuoteCards = async (req, res) => {
 
         axios.get(`https://query1.finance.yahoo.com/v1/finance/search?q=${ticker}`)
             .then((response) => {
-                quotes = response.data.quotes;
+                const quotes = response.data.quotes;
 
                 quotes.map((quote) => {
                     trimQuote(quote);
@@ -37,6 +37,77 @@ const getQuoteCards = async (req, res) => {
     }
 };
 
+const quotedTicker = async (req, res) => {
+    try {
+        const ticker = req.query.ticker;
+
+        axios.get(`https://query1.finance.yahoo.com/v8/finance/chart/${ticker}`)
+            .then((response) => {
+                const chart = response.data.chart.result[0];
+
+                const data = {
+                    symbol: chart.meta.symbol,
+                    type: chart.meta.instrumentType,
+                    marketPrice: chart.meta.regularMarketPrice,
+                    currency: chart.meta.currency,
+                    variation: (chart.meta.regularMarketPrice - chart.meta.previousClose).toFixed(2),
+                    variationPercent: ((chart.meta.regularMarketPrice - chart.meta.previousClose) / chart.meta.previousClose * 100).toFixed(2),
+                    previousClose: chart.meta.previousClose,
+                    open: chart.indicators.quote[0].open[0].toFixed(2),
+                    volume: String(chart.indicators.quote[0].volume.reduce((cur, acc) => acc += cur)),
+                };
+
+                data.variationColor = '#cc3e33';
+
+                if (data.variation > 0) {
+                    data.variation = `+${data.variation}`;
+                    data.variationPercent = `+${data.variationPercent}`;
+                    data.variationColor = '#31c38c';
+                }
+
+                res.status(200).render('quotedTicker', { username: req.session.user.username, data: data });
+            })
+            .catch((err) => {
+                res.status(500).json(err.message);
+            });
+    }
+    catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+};
+
+const sanitizePrices = async (indicators) => {
+    return indicators.quote[0].close.filter((price) => {
+        if (price !== null) {
+            return price;
+        }
+    });
+}
+
+const quotedChart = async (req, res) => {
+    try {
+        const ticker = req.query.ticker;
+        const range = req.query.range;
+        const interval = req.query.interval;
+
+        axios.get(`https://query1.finance.yahoo.com/v8/finance/chart/${ticker}?range=${range}&interval=${interval}`)
+            .then(async (response) => {
+                const chart = response.data.chart.result[0];
+                chart.indicators = await sanitizePrices(chart.indicators);
+
+                res.status(200).json(chart);
+            })
+            .catch((err) => {
+                res.status(500).json(err.message);
+            });
+    }
+    catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+};
+
 module.exports = {
     getQuoteCards,
+    quotedTicker,
+    quotedChart,
 };
--- server/routes.js
+++ server/routes.js
@@ -28,5 +28,7 @@ router.get('/theme', userSettingsController.getUserTheme);
 router.put('/theme', userSettingsController.updateUserTheme);
 
 router.get('/quote/:ticker', quoteController.getQuoteCards);
+router.get('/quoted', quoteController.quotedTicker);
+router.get('/quoted/chart', quoteController.quotedChart);
 
 module.exports = router;
