504928f6ecc840267c253a7c6593937df8a86227
pedrodcarvalho <pedrodc51203@gmail.com>
2023-10-25 22:11:54 -0300

    Balance history and Wallet summary implementation for home page

--- client/public/assets/scripts/home.js
+++ client/public/assets/scripts/home.js
@@ -21,13 +21,30 @@ const loadFinanceApplets = async () => {
 
         dashboard.innerHTML += applet;
     });
+
+    dashboard.style.justifyContent = 'space-between';
 };
 
 const loadVideo = (videoId, iFrame) => {
     iFrame.src = `https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&mute=1&disablekb=1&loop=1&rel=0`;
 };
 
+const setControlsWidth = () => {
+    const controls = document.querySelector('.controls');
+    controls.style.display = 'flex';
+
+    const frameWidth = document.querySelector('.video-container').offsetWidth - 30;
+    controls.style.width = frameWidth + 'px';
+
+    window.addEventListener('resize', () => {
+        const frameWidth = document.querySelector('.video-container').offsetWidth - 30;
+        controls.style.width = frameWidth + 'px';
+    });
+};
+
 const loadLiveNews = async () => {
+    setControlsWidth();
+
     await fetch('/dashboard/live-news').then((response) => response.json()).then((data) => {
         data = data.slice(0, 3);
 
@@ -54,5 +71,62 @@ const loadLiveNews = async () => {
     });
 };
 
+const drawChart = (ctx, labels, prices, accentColor, chartOptions) => {
+    const lineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Balance',
+                data: prices,
+                backgroundColor: [`${accentColor}44`],
+                borderColor: [`${accentColor}`],
+                borderWidth: 1,
+                pointStyle: false,
+                fill: true
+            }]
+        },
+        options: {
+            chartOptions
+        }
+    });
+}
+
+const loadBalanceHistory = async () => {
+    const data = await fetch('/dashboard/balance-history').then((response) => response.json()).then((data) => data);
+
+    const labels = data.map((balance) => {
+        const date = new Date(balance.date);
+        return date.toLocaleDateString(navigator.language, { month: 'short', day: 'numeric' });
+    });
+    const prices = data.map((balance) => balance.balance);
+
+    const ctx = document.getElementById('balance-history-chart');
+    const accentColor = getComputedStyle(document.documentElement).getPropertyValue('--accentColor');
+
+    const chartOptions = {
+        animation: {
+            delay: (context) => {
+                let delay = 0;
+                if (context.type === 'chart' && context.mode === 'default') {
+                    delay = context.dataIndex * 2 + context.datasetIndex * 100;
+                }
+                return delay;
+            }
+        }
+    };
+
+    drawChart(ctx, labels, prices, accentColor, chartOptions);
+};
+
+const loadWalletSummary = async () => {
+    await fetch('/dashboard/wallet-summary').then((response) => response.text()).then((data) => {
+        const walletSummary = document.getElementById('wallet-summary');
+        walletSummary.innerHTML = data;
+    });
+};
+
+loadWalletSummary();
 loadFinanceApplets();
+loadBalanceHistory();
 // loadLiveNews();
--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
 const puppeteer = require('puppeteer');
 
+const User = require('../models/User');
+
+const { convertToUSD } = require('../helpers/currencyConverter');
+
 const getFinanceApplets = async (req, res) => {
     try {
         await axios.get(`https://query1.finance.yahoo.com/v1/finance/screener/predefined/saved?scrIds=${req.query.category}&formatted=true&count=5`)
@@ -55,7 +59,62 @@ const getLiveNews = async (req, res) => {
     }
 };
 
+const getBalanceHistory = async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.session.user.username });
+
+        const balanceHistory = user.balance_history.map((balance) => {
+            return {
+                date: balance.date,
+                balance: balance.balance,
+            };
+        });
+
+        res.status(200).json(balanceHistory);
+    }
+    catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+};
+
+const getWalletSummary = async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.session.user.username });
+
+        const wallet = user.wallet;
+
+        const tickersArray = await Promise.all(wallet.shares.map(async (asset) => {
+            return await axios.get(`https://query1.finance.yahoo.com/v8/finance/chart/${asset.ticker}`)
+                .then((response) => {
+                    if (response.data.chart.result[0].meta.currency !== 'USD') {
+                        return convertToUSD(response.data.chart.result[0].meta.currency, response.data.chart.result[0].meta.regularMarketPrice * asset.shares)
+                            .then((convertedPrice) => {
+                                return convertedPrice * asset.shares;
+                            })
+                            .catch((err) => {
+                                console.log(err);
+                            });
+                    }
+
+                    return response.data.chart.result[0].meta.regularMarketPrice * asset.shares
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }));
+
+        const investedMoney = tickersArray.reduce((accumulator, currentValue) => accumulator + currentValue);
+
+        res.render('walletSummary', { wallet: wallet, investedMoney: investedMoney });
+    }
+    catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+};
+
 module.exports = {
     getFinanceApplets,
     getLiveNews,
+    getBalanceHistory,
+    getWalletSummary,
 };
