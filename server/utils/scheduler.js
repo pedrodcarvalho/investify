09ca3388d04bd94ddb9723aa3cdc8d6b426223b3
pedrodcarvalho <pedrodc51203@gmail.com>
2023-11-09 12:53:42 -0300

    Restrict balance history update twice a day

--- server/utils/scheduler.js
+++ server/utils/scheduler.js
@@ -9,10 +9,22 @@ const storeUserBalance = async () => {
             users.map(async (user) => {
                 const balanceHistory = user.balance_history;
 
-                balanceHistory.push({
-                    balance: user.wallet.balance,
-                    date: new Date(),
-                });
+                if (balanceHistory.length > 0) {
+                    const lastBalance = balanceHistory[balanceHistory.length - 1];
+
+                    const lastBalanceDate = new Date(lastBalance.date);
+                    const currentDate = new Date();
+
+                    const difference = currentDate.getTime() - lastBalanceDate.getTime();
+                    const hoursDifference = difference / (1000 * 3600);
+
+                    if (hoursDifference >= 12) {
+                        balanceHistory.push({
+                            balance: user.wallet.balance,
+                            date: new Date(),
+                        });
+                    }
+                }
 
                 await user.updateOne({ balance_history: balanceHistory });
             });
