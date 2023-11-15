72fc44979983694eab44a708e62a49b72b8ccf80
pedrodcarvalho <pedrodc51203@gmail.com>
2023-11-15 18:35:26 -0300

    Fix balance store to store the entire balance and not only the invested funds

--- server/utils/scheduler.js
+++ server/utils/scheduler.js
@@ -1,5 +1,35 @@
-const User = require('../models/User');
 const cron = require('node-cron');
+const axios = require('axios');
+
+const User = require('../models/User');
+
+const { convertToUSD } = require('../helpers/currencyConverter');
+
+const getInvestedMoney = async (user) => {
+    const wallet = user.wallet;
+
+    const tickersArray = await Promise.all(wallet.shares.map(async (asset) => {
+        return await axios.get(`https://query1.finance.yahoo.com/v8/finance/chart/${asset.ticker}`)
+            .then((response) => {
+                if (response.data.chart.result[0].meta.currency !== 'USD') {
+                    return convertToUSD(response.data.chart.result[0].meta.currency, response.data.chart.result[0].meta.regularMarketPrice * asset.shares)
+                        .then((convertedPrice) => {
+                            return convertedPrice * asset.shares;
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+                }
+
+                return response.data.chart.result[0].meta.regularMarketPrice * asset.shares
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }));
+
+    return tickersArray.reduce((accumulator, currentValue) => accumulator + currentValue);
+};
 
 const storeUserBalance = async () => {
     cron.schedule('* */12 * * *', async () => {
@@ -18,9 +48,11 @@ const storeUserBalance = async () => {
                     const difference = currentDate.getTime() - lastBalanceDate.getTime();
                     const hoursDifference = difference / (1000 * 3600);
 
+                    const investments = await getInvestedMoney(user);
+
                     if (hoursDifference >= 12) {
                         balanceHistory.push({
-                            balance: user.wallet.balance,
+                            balance: user.wallet.balance + investments,
                             date: new Date(),
                         });
                     }
